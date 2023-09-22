aab2df28e4b2c348410f7e94c94e7211f9b37efb
filipegretzky <136389587+filipegretzky@users.noreply.github.com>
2023-09-22 09:38:36 -0300

    Create theme.ts

---
+++ src/assets/styles/theme.ts
@@ -0,0 +1,9 @@
+const theme = {
+  colors: {
+    white: '#fff',
+    forestgreen: 'rgba(4,102,57,1)',
+    ghostgreen: 'rgba(180,224,142,1)',
+    green: 'rgba(0,194,71,1)'
+  },
+  fontSizes: {}
+}
