ff22fd08d8424a66153981603e42b1b8abf763ec
filipegretzky <136389587+filipegretzky@users.noreply.github.com>
2023-09-22 15:26:45 -0300

    add

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,4 +1,38 @@
+import { DivLogin, DivbottonLogin } from './style'
+
 const Login = () => {
-  return <div></div>
+  return (
+    <DivbottonLogin>
+      <DivLogin>
+        <h4>seja bem vindo</h4>
+        <h1>Realize seu login</h1>
+        <div>
+          <label>
+            email
+            <input />
+          </label>
+        </div>
+
+        <div>
+          <label>
+            senha
+            <input />
+          </label>
+        </div>
+
+        <div>
+          <label>
+            <input type="checkbox" />
+          </label>
+          lembre-se de mim
+          <a>esqueci minha senha </a>
+        </div>
+
+        <div>
+          <button>entrar</button>
+        </div>
+      </DivLogin>
+    </DivbottonLogin>
+  )
 }
 export default Login
--- src/pages/login/style.ts
+++ src/pages/login/style.ts
@@ -1,1 +1,16 @@
+import { theme } from '@/assets/styles/theme'
 import styled from 'styled-components'
+
+export const DivbottonLogin = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: green;
+  height: 100vh;
+  width: 100vw;
+`
+export const DivLogin = styled.div`
+  width: 40%;
+  height: 60%;
+  background: ${({ theme }) => theme.colors.white};
+`
